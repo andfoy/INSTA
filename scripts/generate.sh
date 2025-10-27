7b6d671dc10b184adbc1d639c9cce7438aa32133
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2025-10-27 16:02:20 -0500

    Comment out environment setup in generate.sh
    Comment out Python environment setup and script directory variables.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,9 +2,9 @@
 
 USER="$(whoami)"
 
-PYTHON_ENV=/home/${USER}/miniconda3/etc/profile.d/conda.sh
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-source ${PYTHON_ENV}
+#PYTHON_ENV=/home/${USER}/miniconda3/etc/profile.d/conda.sh
+#SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+#source ${PYTHON_ENV}
 
 INPUT="empty"
 OUTPUT="empty"
