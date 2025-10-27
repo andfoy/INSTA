462fb3b7d19a240480076a7f94686907cac0510b
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2025-10-27 15:51:19 -0500

    Add script to batch process multiple inputs

---
+++ batch_process.py
@@ -0,0 +1,16 @@
+import os
+import os.path as osp
+import argparse
+import subprocess
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input-dir', type=str, help='Input dir', required=True)
+    args = parser.parse_args()
+
+    for root, dirs, _ in os.walk(args.input_dir):
+        for dir in dirs:
+            if dir[0] == '.':
+                continue
+            subprocess.run(["bash", "generate.sh", osp.join(root, dir),
+                            osp.join(root, dir)])
